777928c0f9724ee1916b577ae772bdcd85e5f1f2
Alex Gibson <alexgibson@users.noreply.github.com>
2018-03-13 15:27:18 +0000

    Add time (hh:mm) to report dates

--- tests/summary.test.js
+++ tests/summary.test.js
@@ -12,7 +12,7 @@ test('getReportName', () => {
 });
 
 test('getReportDate', () => {
-    expect(summary.getReportDate(1519731247822)).toEqual('2018-02-27');
+    expect(summary.getReportDate(1519731247822)).toEqual('2018-02-27-11:34');
 });
 
 test('format', () => {
