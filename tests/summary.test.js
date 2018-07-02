f0db31b451ec2ebed19aeaa7e4192bb555d23cb5
Alex Gibson <alexgibson@users.noreply.github.com>
2018-07-02 10:11:55 +0100

    Fix summary report formatting due to changes in Lighthouse data

--- tests/summary.test.js
+++ tests/summary.test.js
@@ -20,23 +20,23 @@ test('format', () => {
     let report = JSON.parse(fs.readFileSync('./tests/data/test_report.json'));
 
     expect(summary.format(report)).toEqual({
-        'id': '180308_YK_dcbdf326749ba4db9c418b4407d361a8',
-        'lighthouse': 'https://www.webpagetest.org/lighthouse.php?test=180308_YK_dcbdf326749ba4db9c418b4407d361a8',
+        'id': '180702_BM_be2477c2c7d2380051601f223f39d3c2',
+        'lighthouse': 'https://www.webpagetest.org/lighthouse.php?test=180702_BM_be2477c2c7d2380051601f223f39d3c2',
         'metrics': {
-            'bytesIn': 1213620,
-            'documentComplete': 7847,
-            'fullyLoaded': 8184,
-            'requests': 49
+            'bytesIn': 1051755,
+            'documentComplete': 8920,
+            'fullyLoaded': 9976,
+            'requests': 53
         },
         'scores': {
-            'accessibility': 81,
-            'bestpractices': 75,
+            'accessibility': 79,
+            'bestpractices': 93,
             'performance': 80,
-            'pwa': 54,
-            'seo': 77
+            'pwa': 58,
+            'seo': 90
         },
-        'summary': 'https://www.webpagetest.org/results.php?test=180308_YK_dcbdf326749ba4db9c418b4407d361a8',
-        'trend': 'careers_mozilla_org_en-US_.trend.json',
-        'url': 'https://careers.mozilla.org/en-US/'
+        'summary': 'https://www.webpagetest.org/results.php?test=180702_BM_be2477c2c7d2380051601f223f39d3c2',
+        'trend': 'irlpodcast_org_.trend.json',
+        'url': 'https://irlpodcast.org/'
     });
 });
