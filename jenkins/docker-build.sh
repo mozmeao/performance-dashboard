d28b86e55fb98163231b7bf6bfd45b50c35e2306
Paul McLanahan <pmac@mozilla.com>
2018-10-15 13:59:42 -0400

    Fix Netlify env var for Docker build
    Missed this in the previous change

--- jenkins/docker-build.sh
+++ jenkins/docker-build.sh
@@ -6,7 +6,7 @@ DOCKER_TAG="meao-perf-dashboard:latest"
 
 docker build --pull -t "$DOCKER_TAG" -f jenkins/Dockerfile .
 docker run -i --rm \
-           -e NETLIFY_TOKEN \
+           -e NETLIFY_AUTH_TOKEN \
            -e NETLIFY_SITE_ID \
            -e WPT_API_KEY \
            "$DOCKER_TAG"
